81c35f3c0cd583eb3d024422ca278a081cf71e69
HEdingfield <hylton+git@groupagree.com>
2017-05-27 15:44:30 -0700

    Implemented speak method; more string manipulation

--- Program.cs
+++ Program.cs
@@ -4,11 +4,36 @@ namespace ConsoleApplication
 {
     public class Program
     {
+
+        public static string speak(string s1, string s2, string intensity = "")
+        {
+            string output = $"{s1}, {s2}.";
+            if (intensity == "loud")
+            {
+                output = output.ToUpper().Replace(".", "!");
+            }
+            else if (intensity == "quiet")
+            {
+                output = output.ToLower().Replace(".", "...");
+            }
+            return output;
+        }
+
         public static void Main()
         {
             var greeting = "Welcome back";
             var name = "Agent Cooper";
-            Console.WriteLine($"{greeting}, {name}!");
+            var combined = greeting + ", " + name + ".";
+            Console.WriteLine(combined);
+            Console.WriteLine(combined.Replace(name, "BOB"));
+            Console.WriteLine($"{greeting} to string interpolation, {name}! Apparently this has the best performance.");
+            // NOTE: Substring parameters are (starting index, length of substring)
+            Console.WriteLine(combined.ToUpper().Substring(20, 4));
+
+            Console.WriteLine(speak(greeting, name));
+            Console.WriteLine(speak(greeting, name, "loud"));
+            Console.WriteLine(speak(greeting, name, "quiet"));
+            Console.WriteLine(speak(greeting, name, "dumb"));
         }
     }
 }
